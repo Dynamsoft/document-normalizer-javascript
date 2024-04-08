b28968a7994744e6f514ba99bde0ce578e54dce5
DMGithubPublisher <github@dynamsoft.com>
2024-04-08 02:01:45 -0700

    2.2.10

--- dist/types/interface/normalizedimagesresult.d.ts
+++ dist/types/interface/normalizedimagesresult.d.ts
@@ -1,8 +1,10 @@
-import { CapturedResult, ImageTag } from "dynamsoft-core";
+import { ImageTag } from "dynamsoft-core";
 import { NormalizedImageResultItem } from "./NormalizedImageResultItem";
-export interface NormalizedImagesResult extends CapturedResult {
+export interface NormalizedImagesResult {
     readonly originalImageHashId: string;
     readonly originalImageTag: ImageTag;
+    readonly errorCode: number;
+    readonly errorString: string;
     normalizedImageResultItems: Array<NormalizedImageResultItem>;
 }
 declare module "dynamsoft-capture-vision-router" {
